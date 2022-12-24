ba019727f896f8220263b87385d8fa7e37513903
lightning-commits <402320079@qq.com>
2022-12-24 22:53:59 +0800

    add permission list

--- api/request.js
+++ api/request.js
@@ -14,20 +14,20 @@ const urls = class{
 		let queryRecordByUserId = `${userUrl}bill-record/queryRecordByUserId`
 		// 登录添加用户信息
 		let addUser = `${userUrl}bill-user/addUser`
-		
+		//查询用户账单自定义
 		let queryBillDetailsCustomize=`${detailsUrl}billBookInfoController/queryBillDetailsCustomize`
-		
+		//查询默认账单
 		let queryDefaultBillByUserId=`${userUrl}billBookInfoController/queryDefaultBillByUserId`
-		
+		//查询用户所有账单
 		let queryBillByUserId=`${userUrl}bill-manage/queryBillByUserId`
-		
+		//查询年度账单
 		let queryAnnualSummaryReportList = `${gatprojectUrl}billBookAnnualSummaryController/queryAnnualSummaryReportList`
-		
+		//修改默认账单
 		let updateDefaultBill = `${userUrl}/bill-manage/updateDefaultBill`
 		
 		return {queryUser,queryRecordByUserId,
 		queryBillDetailsCustomize,queryDefaultBillByUserId,
-		queryBillByUserId,queryAnnualSummaryReportList}
+		queryBillByUserId,queryAnnualSummaryReportList,updateDefaultBill}
 	}
 }
 export default urls 
